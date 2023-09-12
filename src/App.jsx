84be9f9b184791ed21d61129048473af8d08d2a4
kujtimsadiku <kujtim_sadiku@hotmail.com>
2023-09-12 16:10:09 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>navigation bar</div>
+      <div>Front end</div>
+    </>
+  );
+}
+
+export default App;
