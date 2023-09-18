7ef351abf632a650b3d5379f329d63837b08b313
kujtimsadiku <kujtim_sadiku@hotmail.com>
2023-09-18 15:47:27 +0300

    styling

--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -1,7 +1,8 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
-import "./index.css";
+import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
