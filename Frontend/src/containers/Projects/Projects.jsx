e5cd2696680191e47be35380c5ec162556134f3d
kujtimsadiku <kujtim_sadiku@hotmail.com>
2023-09-20 22:06:44 +0300

    Project icons design

--- Frontend/src/containers/Projects/Projects.jsx
+++ Frontend/src/containers/Projects/Projects.jsx
@@ -25,6 +25,19 @@ const Projects = () => {
 
   const handleWorkFilter = (item) => {
     setActiveFilter(item);
+    setAnimatedCard([{ y: 100, opacity: 0 }]);
+
+    setTimeout(() => {
+      setAnimatedCard([{ y: 0, opacity: 1 }]);
+
+      if (item === "All") {
+        setFilterProject(projects);
+      } else {
+        setFilterProject(
+          projects.filter((project) => project.tags.includes(item))
+        );
+      }
+    }, 500);
   };
 
   return (
@@ -33,7 +46,7 @@ const Projects = () => {
         My <span>Projects</span>
       </h2>
       <div className="app__projects-filter">
-        {["UI/UX", "Web app", "Mobile app", "React JS", "All"].map(
+        {["UI/UX", "Web App", "Mobile App", "React JS", "All"].map(
           (item, index) => (
             <div
               key={index}
@@ -54,8 +67,51 @@ const Projects = () => {
       >
         {filterProject.map((project, index) => (
           <div className="app__project-item app__flex" key={index}>
-            <div className="app__project-img app_flex">
-              <img src={urlFor(project.imgUrl)} alt={project.name}></img>
+            <div className="app__project-img app__flex">
+              <img src={urlFor(project.imgUrl)} alt={project.name} />
+              <motion.div
+                whileHover={{ opacity: [0, 1] }}
+                transition={{
+                  duration: 0.5,
+                  ease: "easeInOut",
+                  staggerChildren: 0.5,
+                }}
+                className="app__project-hover app__flex"
+              >
+                <a href={project.projectLink} target="_blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{
+                      duration: 0.5,
+                    }}
+                    className="app__flex"
+                  >
+                    <AiFillEye />
+                  </motion.div>
+                </a>
+                <a href={project.codeLink} target="_blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{
+                      duration: 0.5,
+                    }}
+                    className="app__flex"
+                  >
+                    <AiFillGithub />
+                  </motion.div>
+                </a>
+              </motion.div>
+            </div>
+            <div className="app__project-content app__flex">
+              <h4 className="bold-text">{project.title}</h4>
+              <p className="p-text" style={{ marginTop: 10 }}>
+                {project.description}
+              </p>
+              <div className="app__project-tag app__flex">
+                <p className="p-text">{project.tags[0]}</p>
+              </div>
             </div>
           </div>
         ))}
@@ -64,4 +120,4 @@ const Projects = () => {
   );
 };
 
-export default Projects;
+export default AppWrap(Projects, "projects");
