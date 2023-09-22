5865f303c3e7d5044a01e0fee683143215bb57e3
kujtimsadiku <kujtim_sadiku@hotmail.com>
2023-09-22 23:43:57 +0300

    About me section added text

--- Frontend/src/containers/About/About.jsx
+++ Frontend/src/containers/About/About.jsx
@@ -22,13 +22,37 @@ const About = () => {
 
   return (
     <>
-      <h2 className="head-text">
-        I Know That
-        <span> Good Design </span>
-        <br />
-        means
-        <span> Good Business</span>
-      </h2>
+      <div className="app__about-me">
+        <h2 className="head-text">
+          I Know That
+          <span> Good Design </span>
+          <br />
+          means
+          <span> Good Business</span>
+        </h2>
+
+        <h4 className="head-text">
+          About me
+          <p className="p-text">
+            I embarked on my coding journey in 2022 with no prior experience.
+            <br />
+            My foundation was forged at Hive Helsinki, where I honed
+            problem-solving skills, optimized code, and embraced software design
+            principles.
+            <br />
+            <br />
+            As an aspiring Full Stack Developer, I recently completed the Full
+            Stack Open Course at the University of Helsinki.
+            <br />
+            <br />
+            Proficient in frontend technologies like HTML, CSS, JavaScript, and
+            React.js,
+            <br />I equally excel in backend technologies, including Node.js,
+            Express, MongoDB, REST API, GraphQL, Sanity, JSON, and Redux
+            Toolkit/Query.
+          </p>
+        </h4>
+      </div>
 
       <div className="app__profiles">
         {abouts.map((about, index) => (
